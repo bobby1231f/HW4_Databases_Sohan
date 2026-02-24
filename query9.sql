a0204f68a1388587f8f9a85ecd40f9193797cafa
bobby1231f <sohoocelery@gmail.com>
2026-02-23 20:28:39 -0800

    Update query9.sql

--- query9.sql
+++ query9.sql
@@ -8,3 +8,5 @@ JOIN tracks t ON al.AlbumId = t.AlbumId
 JOIN invoice_items ii ON t.TrackId = ii.TrackId
 GROUP BY ar.Name
 ORDER BY RevenueRank;
+
+--This query builds a revenue leaderboard of all artists in the store. It traces the full path from an artist to their albums to the tracks on those albums to actual sales of those tracks, then calculates how much total revenue each artist has generated. It then uses a window function to rank every artist from highest to lowest earner.
